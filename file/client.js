d548e36625fefa79104ab196a5fa238f398861a7
RadityaP <prirahmadian@gmail.com>
2022-12-01 22:39:56 +0700

    fix: change sort of clients

--- file/client.js
+++ file/client.js
@@ -1,5 +1,133 @@
 export const photos = [
   {
+    src: require('/public/assets/clients/pertamina.png'),
+    width: 1,
+    height: 1,
+    title: 'Pertamina',
+  },
+  {
+    src: require('/public/assets/clients/pln.png'),
+    width: 1,
+    height: 1,
+    title: 'PLN',
+  },
+  {
+    src: require('/public/assets/clients/hyundai.png'),
+    width: 1,
+    height: 1,
+    title: 'Hyundai',
+  },
+  {
+    src: require('/public/assets/clients/itdri.png'),
+    width: 1,
+    height: 1,
+    title: 'ITDRI by Telkom Indonesia',
+  },
+  {
+    src: require('/public/assets/clients/bankbjb.png'),
+    width: 1,
+    height: 1,
+    title: 'Bank BJB',
+  },
+  {
+    src: require('/public/assets/clients/dirjen-kemenkeu.png'),
+    width: 1,
+    height: 1,
+    title: 'Direktorat Jenderal Kekayaan Negara Kemenkeu',
+  },
+  {
+    src: require('/public/assets/clients/inspektorat-jawabarat.png'),
+    width: 1,
+    height: 1,
+    title: 'Inspektorat Jawa Barat',
+  },
+  {
+    src: require('/public/assets/clients/surveyor.png'),
+    width: 1,
+    height: 1,
+    title: 'Surveyor Indonesia',
+  },
+
+  {
+    src: require('/public/assets/clients/greenpeace.png'),
+    width: 1,
+    height: 1,
+    title: 'Greenpeace',
+  },
+  {
+    src: require('/public/assets/clients/inpoin.png'),
+    width: 1,
+    height: 1,
+    title: 'Inpoin by Telkom Indonesia',
+  },
+  {
+    src: require('/public/assets/clients/qip.png'),
+    width: 1,
+    height: 1,
+    title: 'Qip',
+  },
+  {
+    src: require('/public/assets/clients/pemkot-cimahi.png'),
+    width: 1,
+    height: 1,
+    title: 'Pemerintah Kota Cimahi',
+  },
+  {
+    src: require('/public/assets/clients/pemkab-bandung.png'),
+    width: 1,
+    height: 1,
+    title: 'Pemerintah Kabupaten Bandung',
+  },
+  {
+    src: require('/public/assets/clients/schlumberger.png'),
+    width: 1,
+    height: 1,
+    title: 'PT. Schlumberger',
+  },
+  {
+    src: require('/public/assets/clients/paragon.png'),
+    width: 1,
+    height: 1,
+    title: 'PT. Paragon',
+  },
+  {
+    src: require('/public/assets/clients/tokopedia.png'),
+    width: 1,
+    height: 1,
+    title: 'Tokopedia',
+  },
+
+  {
+    src: require('/public/assets/clients/telkom-university.png'),
+    width: 1,
+    height: 1,
+    title: 'Telkom University',
+  },
+  {
+    src: require('/public/assets/clients/meiyuave.png'),
+    width: 1,
+    height: 1,
+    title: 'Meiyume',
+  },
+  {
+    src: require('/public/assets/clients/swisbel.png'),
+    width: 1,
+    height: 1,
+    title: 'Swissbell-Resort Dago Hotel Bandung',
+  },
+  {
+    src: require('/public/assets/clients/universitas-bakthi.png'),
+    width: 1,
+    height: 1,
+    title: 'Universitas Bhakti Kencana',
+  },
+  {
+    src: require('/public/assets/clients/bareskim.png'),
+    width: 1,
+    height: 1,
+    title: 'Bareskim',
+  },
+  {
     src: require('/public/assets/clients/agree.png'),
     width: 1,
     height: 1,
@@ -12,29 +140,12 @@ export const photos = [
     title: 'AYIC Unpad 2020',
   },
   {
-    src: require('/public/assets/clients/baduma.png'),
-    width: 1,
-    height: 1,
-    title: 'Baduma Shoes',
-  },
-  {
-    src: require('/public/assets/clients/bankbjb.png'),
-    width: 1,
-    height: 1,
-    title: 'Bank BJB',
-  },
-  {
     src: require('/public/assets/clients/dhio.png'),
     width: 1,
     height: 1,
     title: 'Dhio Adhinugra Verde Resto and Lounge',
   },
-  {
-    src: require('/public/assets/clients/dirjen-kemenkeu.png'),
-    width: 1,
-    height: 1,
-    title: 'Direktorat Jenderal Kekayaan Negara Kemenkeu',
-  },
+
   {
     src: require('/public/assets/clients/emina.png'),
     width: 1,
@@ -59,24 +170,7 @@ export const photos = [
     height: 1,
     title: 'HMI Cabang Bandung',
   },
-  {
-    src: require('/public/assets/clients/inpoin.png'),
-    width: 1,
-    height: 1,
-    title: 'Inpoin by Telkom Indonesia',
-  },
-  {
-    src: require('/public/assets/clients/inspektorat-jawabarat.png'),
-    width: 1,
-    height: 1,
-    title: 'Inspektorat Jawa Barat',
-  },
-  {
-    src: require('/public/assets/clients/itdri.png'),
-    width: 1,
-    height: 1,
-    title: 'ITDRI by Telkom Indonesia',
-  },
+
   {
     src: require('/public/assets/clients/alisa.png'),
     width: 1,
@@ -96,12 +190,7 @@ export const photos = [
     height: 1,
     title: 'Sinestesia',
   },
-  {
-    src: require('/public/assets/clients/hieva.png'),
-    width: 1,
-    height: 1,
-    title: 'Hieva',
-  },
+
   {
     src: require('/public/assets/clients/pef-2018.png'),
     width: 1,
@@ -115,43 +204,20 @@ export const photos = [
     height: 1,
     title: 'PEF 2019',
   },
-  {
-    src: require('/public/assets/clients/qip.png'),
-    width: 1,
-    height: 1,
-    title: 'Qip',
-  },
+
   {
     src: require('/public/assets/clients/savaravat.png'),
     width: 1,
     height: 1,
     title: 'Savaravat',
   },
-
-  {
-    src: require('/public/assets/clients/telkom-university.png'),
-    width: 1,
-    height: 1,
-    title: 'Telkom University',
-  },
   {
     src: require('/public/assets/clients/psm-unpad.png'),
     width: 1,
     height: 1,
-    title: 'Paduan Suara Mahasiswa Univeesitas Padjadjaran',
+    title: 'Paduan Suara Mahasiswa Universitas Padjadjaran',
   },
-  {
-    src: require('/public/assets/clients/pemkab-bandung.png'),
-    width: 1,
-    height: 1,
-    title: 'Pemerintah Kabupaten Bandung',
-  },
-  {
-    src: require('/public/assets/clients/pemkot-cimahi.png'),
-    width: 1,
-    height: 1,
-    title: 'Pemerintah Kota Cimahi',
-  },
+
   {
     src: require('/public/assets/clients/prabu-unpad.png'),
     width: 1,
@@ -164,42 +230,14 @@ export const photos = [
     height: 1,
     title: 'PT. Bringin Gigantara',
   },
-  {
-    src: require('/public/assets/clients/paragon.png'),
-    width: 1,
-    height: 1,
-    title: 'PT. Paragon',
-  },
-  {
-    src: require('/public/assets/clients/schlumberger.png'),
-    width: 1,
-    height: 1,
-    title: 'PT. Schlumberger',
-  },
+
   {
     src: require('/public/assets/clients/sma-merdeka.png'),
     width: 1,
     height: 1,
     title: 'SMA Merdeka Soreang',
   },
-  {
-    src: require('/public/assets/clients/swisbel.png'),
-    width: 1,
-    height: 1,
-    title: 'Swissbell-Resort Dago Hotel Bandung',
-  },
-  {
-    src: require('/public/assets/clients/tokopedia.png'),
-    width: 1,
-    height: 1,
-    title: 'Tokopedia',
-  },
-  {
-    src: require('/public/assets/clients/universitas-bakthi.png'),
-    width: 1,
-    height: 1,
-    title: 'Universitas Bhakti Kencana',
-  },
+
   {
     src: require('/public/assets/clients/universitas-islam-bandung.png'),
     width: 1,
@@ -212,42 +250,7 @@ export const photos = [
     height: 1,
     title: 'WR Steak Coffee',
   },
-  {
-    src: require('/public/assets/clients/bareskim.png'),
-    width: 1,
-    height: 1,
-    title: 'Bareskim',
-  },
-  {
-    src: require('/public/assets/clients/surveyor.png'),
-    width: 1,
-    height: 1,
-    title: 'Surveyor Indonesia',
-  },
-  {
-    src: require('/public/assets/clients/greenpeace.png'),
-    width: 1,
-    height: 1,
-    title: 'Greenpeace',
-  },
-  {
-    src: require('/public/assets/clients/hyundai.png'),
-    width: 1,
-    height: 1,
-    title: 'Hyundai',
-  },
-  {
-    src: require('/public/assets/clients/pln.png'),
-    width: 1,
-    height: 1,
-    title: 'PLN',
-  },
-  {
-    src: require('/public/assets/clients/pertamina.png'),
-    width: 1,
-    height: 1,
-    title: 'Pertamina',
-  },
+
   {
     src: require('/public/assets/clients/pribadi.png'),
     width: 1,
@@ -255,15 +258,21 @@ export const photos = [
     title: 'Pribadi Bilingual Boarding School Bandung',
   },
   {
-    src: require('/public/assets/clients/meiyuave.png'),
-    width: 1,
-    height: 1,
-    title: 'Meiyuave',
-  },
-  {
     src: require('/public/assets/clients/cakrawala.png'),
     width: 1,
     height: 1,
     title: 'Cakrawala Outbond',
+  },
+  {
+    src: require('/public/assets/clients/baduma.png'),
+    width: 1,
+    height: 1,
+    title: 'Baduma Shoes',
+  },
+  {
+    src: require('/public/assets/clients/hieva.png'),
+    width: 1,
+    height: 1,
+    title: 'Hieva',
   },
 ];
